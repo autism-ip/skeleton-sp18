38f40a1227d1fa05468aadc3ee655c5e2afe8d5b
Zen <142089698+autism-ip@users.noreply.github.com>
2025-08-20 22:26:50 +0800

    On branch master 	modified:   hw1/synthesizer/ArrayRingBuffer.java

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,13 +1,8 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
 package synthesizer; 
 
 import java.util.Iterator;
 
-
-//TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
+public class ArrayRingBuffer extends AbstractBoundedQueue {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -19,11 +14,6 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
-        //       this.capacity should be set appropriately. Note that the local variable
-        //       here shadows the field we inherit from AbstractBoundedQueue, so
-        //       you'll need to use this.capacity to set the capacity.
         first = 0;
         last = 0;
         fillCount = 0;
@@ -39,7 +29,6 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      */
     @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
         if (isFull()) {
             throw new RuntimeException("Ring Buffer overflow");
         }
