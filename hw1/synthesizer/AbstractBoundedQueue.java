28bee6bf650d511b3797bd236156499f0f0c3f79
Zen <142089698+autism-ip@users.noreply.github.com>
2025-08-20 22:04:05 +0800

    On branch master 	modified:   hw1/synthesizer/AbstractBoundedQueue.java

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -10,7 +10,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount() {
         return fillCount;
     }
-    public abstract void enqueue(T x);
-    public abstract T peek();
-    public abstract T dequeue();
 }
